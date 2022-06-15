946aa23700e4efe2e6a27ada34b49f9feba086b6
abdul-osman-mecca <39681296+osmaniacattack@users.noreply.github.com>
2022-06-14 19:22:01 -0700

    Updated project notes

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+// Get all panels as a nodeList array
+
 const panels = document.querySelectorAll('.panel');
+
+// Iterate through panels and add the event listener 'click' that removes the active class 
+// and then adds it to the specific panel that was clicked.
 
 panels.forEach((panel) => {
     panel.addEventListener('click', () => {
